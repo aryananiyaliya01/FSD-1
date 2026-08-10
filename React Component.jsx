299db8eab16bb21460bc5e7b4edb5181781677e2
Aniyaliya Aryan <aryananiyaliya@gmail.com>
2026-08-10 22:48:56 +0530

    Add Welcome component to React application

---
+++ React Component.jsx
@@ -0,0 +1,18 @@
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to React</h1>
+            <p>Learning Full Stack Development</p>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome />
+        </div>
+    );
+}
+
+export default App;
